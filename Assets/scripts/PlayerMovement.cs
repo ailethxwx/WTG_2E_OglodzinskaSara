fd22d162f2a55e87cae4e73e13eb0e703534a0fe
ailethxwx <oglodzinskasara@gmail.com>
2025-01-12 20:31:22 +0100

    init

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -9,12 +9,14 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     bool jump = false;
+    
 
 
     // Update is called once per frame
     void Update()
     {
        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
 
        if (Input.GetButtonDown("Jump"))
        {
